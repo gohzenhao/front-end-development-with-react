f69601fc73ba2f02eeee193fbad0c254210f1ed4
gohzenhao <gohzenhao@gmail.com>
2020-05-08 01:10:57 +1000

    Added comment modal using react-redux-form

--- src/components/CommentComponent.js
+++ src/components/CommentComponent.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import { Link } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, Row, Col, Label} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
@@ -101,76 +98,4 @@ class CommentForm extends Component {
     }
 };
 
-    function RenderDish({dish}) {
-        if (dish != null)
-            return(
-                <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-                </div>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-
-    function RenderComments({comments}) {
-        // const comments = c.map((comment) => {
-
-        // });
-        if (comments != null) {
-            const comment = comments.map((c) => {
-                return(
-
-                        <ul className="list-unstyled" key={c.id}>
-                            <li>{c.comment}</li>
-                            <li>- - {c.author} {c.date}</li>
-                        </ul>
-                );
-            });
-
-            return (
-                <div className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                {comment}
-                <CommentForm/>
-                </div>
-            );
-         }
-        else 
-            return(
-                <div></div>
-            );
-    }
-
-    const DishDetail = (props) => {
-        return (
-            <div>
-            <div className="row">
-                <Breadcrumb>
-
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
-            </div>
-            </div>
-        );
-    }
-    
-
-
-export default DishDetail;
+export default CommentForm;
